103c54e5c5fefbb80fda311791002140a31670cc
Martin Paju <fezzzle@gmail.com>
2023-03-19 20:14:50 +0200

    feat: Add sorting method

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,17 +1,31 @@
 import TableHead from "./TableHead";
 import orderTableColumns from "../util/tableutil";
 import TableBody from "./TableBody";
-import React from "react";
+import React, {useState} from "react";
 import {Orders} from "../types/types";
 
-const Table = ({data}: {data: Orders}) => {
+const Table = ({data}: { data: Orders }) => {
+
+    const [tableData, setTableData] = useState(data);
+    const handleSorting = (sortField: string | number, sortOrder: string) => {
+        if (sortField) {
+            const sorted = [...tableData].sort((a, b) => {
+                const sortValueA = a[sortField] || "";
+                const sortValueB = b[sortField] || "";
+                return sortOrder === "asc"
+                    ? sortValueA.toString().localeCompare(sortValueB.toString(), "en", {numeric: true})
+                    : sortValueB.toString().localeCompare(sortValueA.toString(), "en", {numeric: true});
+            });
+            setTableData(sorted);
+        }
+    };
     return (
         <table
             aria-label="Orders data"
             className="max-w-fit bg-white"
         >
-            <TableHead columns={orderTableColumns}/>
-            <TableBody tableData={data} columns={orderTableColumns}/>
+            <TableHead columns={orderTableColumns} handleSorting={handleSorting}/>
+            <TableBody tableData={tableData} columns={orderTableColumns}/>
         </table>
     )
 }
