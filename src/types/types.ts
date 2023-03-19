2d022fb149b667df3d64b2af2f4fedf8351219b6
Martin Paju <fezzzle@gmail.com>
2023-03-19 18:24:19 +0200

    chore: Update types

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,6 +5,48 @@ export interface Order {
   quantity: string;
   status: "New" | "In transit" | "Delivered";
   date: string;
+  [key: string]: any;
 }
 
 export type Orders = Order[];
+
+export interface CurrencyMap {
+    [key: string]: string;
+}
+
+export type Column = {
+  label: string;
+  accessor: string;
+};
+
+const orderTableColumns: Column[] = [
+  {
+    label: "Order ID",
+    accessor: "id",
+  },
+  {
+    label: "Total",
+    accessor: "total",
+  },
+  {
+    label: "Currency",
+    accessor: "currency",
+  },
+  {
+    label: "Quantity",
+    accessor: "quantity",
+  },
+  {
+    label: "Status",
+    accessor: "status",
+  },
+  {
+    label: "Date",
+    accessor: "date",
+  },
+];
+
+export default orderTableColumns;
+
+
+
