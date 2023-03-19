93f999d757cce0b9c913bc11fab77b92dce77690
Martin Paju <fezzzle@gmail.com>
2023-03-19 19:29:10 +0200

    feat: Render correct date format in the table

--- src/components/TableBody.tsx
+++ src/components/TableBody.tsx
@@ -1,78 +1,45 @@
 import React from 'react'
-
 import {Orders, Column, CurrencyMap} from "../types/types";
-
-
-// const TableBody = ({tableData, columns}: { tableData: Orders, columns: Column[] }) => {
-//     const currencies: CurrencyMap = {
-//         EUR: "€",
-//         USD: "$",
-//     };
-//     return (
-//         <tbody>
-//         {tableData.map((data) => {
-//             return (
-//                 <tr className="border-b-2" key={data.id}>
-//                     {columns.map(({label, accessor}) => {
-//                         const tableData = data[accessor] ? data[accessor] : "N/A";
-//                         if (label.toLowerCase() === 'status') {
-//                             return (
-//                                 <td className="px-4 py-2" key={accessor}>
-//                                     <span className="bg-gray-200 rounded-full py-2 px-2">{tableData}</span>
-//                                 </td>
-//                             );
-//                         }
-//                         console.log("data", data.currency)
-//                         if (data.currency) {
-//                             return <td className="px-4 py-2"
-//                                        key={accessor}>{tableData} {currencies[data.currency] ? currencies[data.currency] : tableData.currency}</td>;
-//                         } else {
-//                             return <td className="px-4 py-2" key={accessor}>{tableData}</td>;
-//                         }
-//                     })}
-//                 </tr>
-//             );
-//         })}
-//         </tbody>
-//     );
-// };
-
+import getFormattedDateStamp from '../util/formatDate'
 
 const TableBody = ({tableData, columns}: { tableData: Orders, columns: Column[] }) => {
-  const currencies: CurrencyMap = {
-    EUR: "€",
-    USD: "$",
-  };
+    const currencies: CurrencyMap = {
+        EUR: "€",
+        USD: "$",
+    };
 
-  return (
-    <tbody>
-      {tableData.map((data) => {
-        return (
-          <tr className="border-b-2" key={data.id}>
-            {columns.map(({label, accessor}) => {
-              const tableData = data[accessor] ? data[accessor] : "N/A";
+    return (
+        <tbody>
+        {tableData.map((data) => {
+            return (
+                <tr className="border-b-2" key={data.id}>
+                    {columns.map(({label, accessor}) => {
+                        const tableData = data[accessor] ? data[accessor] : "N/A";
 
-              if (accessor === "total" && data.currency) {
-                return (
-                  <td className="px-4 py-2" key={accessor}>
-                    {currencies[data.currency] ? currencies[data.currency] : data.currency}{tableData}
-                  </td>
-                );
-              } else if (label.toLowerCase() === "status") {
-                return (
-                  <td className="px-4 py-2" key={accessor}>
-                    <span className="bg-gray-200 rounded-full py-2 px-2">{tableData}</span>
-                  </td>
-                );
-              } else {
-                return <td className="px-4 py-2" key={accessor}>{tableData}</td>;
-              }
-            })}
-          </tr>
-        );
-      })}
-    </tbody>
-  );
+                        if (accessor === "total" && data.currency) {
+                            return (
+                                <td className="px-4 py-2" key={accessor}>
+                                    {currencies[data.currency] ? currencies[data.currency] : data.currency}{tableData}
+                                </td>
+                            );
+
+                        } else if (label.toLowerCase() === "status") {
+                            return (
+                                <td className="px-4 py-2" key={accessor}>
+                                    <span className="bg-gray-200 rounded-full py-2 px-2">{tableData}</span>
+                                </td>
+                            );
+                        } else if (accessor === "date") {
+                            return <td className="px-4 py-2" key={accessor}>{getFormattedDateStamp(tableData)}</td>;
+                        } else {
+                            return <td className="px-4 py-2" key={accessor}>{tableData}</td>;
+                        }
+                    })}
+                </tr>
+            );
+        })}
+        </tbody>
+    );
 };
 
 
