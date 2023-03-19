004a4995e16f7337d3ce9f364d8c39fca357e664
Martin Paju <fezzzle@gmail.com>
2023-03-19 18:23:59 +0200

    feat: Add table column names with accessors and labels

---
+++ src/util/tableutil.js
@@ -0,0 +1,10 @@
+const ordersTableColumns = [
+    {label: "Order ID", accessor: "id"},
+    {label: "Order date", accessor: "date"},
+    {label: "Order total", accessor: "total"},
+    {label: "Quantity", accessor: "quantity"},
+    {label: "Status", accessor: "status"},
+];
+
+
+export default ordersTableColumns;
